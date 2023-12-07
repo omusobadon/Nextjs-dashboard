9dfb931621f386990b66778f96952d94a0c0879c
yaffu-kitune <22cc0203@jec.ac.jp>
2023-12-08 02:24:07 +0900

    upload file

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+  // pages/dashboard/page.tsx
+import { Component } from '@/components/component/component'
+
+export default function Page() {
+  return (
+    <div>
+      {/* その他のコンテンツ */}
+      <Component />
+      {/* その他のコンテンツ */}
+    </div>
+  );
+}
