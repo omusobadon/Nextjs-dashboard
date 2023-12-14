1113c6666899333f06accfdc21a47ad255d8d3b4
yaffu-kitune <22cc0203@jec.ac.jp>
2023-12-15 04:34:46 +0900

    update

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -15,28 +14,12 @@ export const authConfig = {
         return false;
       } else if (isLoggedIn) {
         return Response.redirect(new URL("/dashboard", request.nextUrl));
-=======
-import type { NextAuthConfig } from 'next-auth';
- 
-export const authConfig = {
-  pages: {
-    signIn: '/login',
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
->>>>>>> test
+
       }
       return true;
     },
   },
-<<<<<<< HEAD
+
   providers: [
     CredentialsProvider({
       async authorize(credentials) {
@@ -51,7 +34,3 @@ export const authConfig = {
   ],
    // Add providers with an empty array for now
 } as NextAuthConfig;
-=======
-  providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
->>>>>>> test
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,7 @@
 import NextAuth from 'next-auth';
-<<<<<<< HEAD
 import { authConfig } from "./auth.config"
 
 export const { auth: middleware } = NextAuth(authConfig)
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-=======
-export { auth as default } from "./auth"
- 
-export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
->>>>>>> test
 };
